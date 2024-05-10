3e90b1b8b7d734c63a95edf8e127f115f0451a78
donnate <cdonnat@uchicago.edu>
2024-05-10 10:57:07 -0500

    adding bash

---
+++ compute_mean.py
@@ -0,0 +1,19 @@
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--namefile', type=str)
+parser.add_argument('--seed', type=int)
+args = parser.parse_args()
+
+np.random.seed(args.seed)
+#### Add code here that samples a random vector of size 100, and returns the mean
+
+result = 0
+##
+
+with open(args.namefile, 'w') as file:
+    # Convert the number to a string and write it to the file
+    file.write(str(result))
+
+print("The number has been written to " + args.namefile)
---
+++ my_first_bash_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=array_compute_mean
+#SBATCH --output=logs/array_mean_compute_%A_%a.out
+#SBATCH --error= logs/array_mean_compute_%A_%a.err
+#SBATCH --array=1-20
+#SBATCH --time=1:00:00
+#SBATCH --partition=caslake
+#SBATCH --mem=20G
+#SBATCH --account=pi-cdonnat
+
+# Print the task id.
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "My SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
+job_id=$SLURM_ARRAY_JOB_ID
+result_file="${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
+echo "result file is ${result_file}"
+cd $SCRATCH/$USER/Github_tutorial
+
+module load python/anaconda-2021.05
+python3 compute_mean.py --namefile $result_file --seed $SLURM_ARRAY_TASK_ID
